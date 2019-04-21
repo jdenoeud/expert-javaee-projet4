efed03aba6289e692b54acefd939d5bd37a81f01
DENOEUD <justine.denoeud@capgemini.com>
2019-04-21 11:55:10 +0200

    Clean myerp-model and myerp-consumer directories

--- src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/SpringRegistry.java
+++ src/myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/SpringRegistry.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
 import com.dummy.myerp.business.contrat.BusinessProxy;
 import com.dummy.myerp.business.impl.TransactionManager;
 
--- src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
+++ src/myerp-business/src/test/java/com/dummy/myerp/business/impl/manager/ComptabiliteManagerImplTest.java
@@ -4,59 +4,17 @@ import java.math.BigDecimal;
 import java.util.Date;
 
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
-
 import com.dummy.myerp.model.bean.comptabilite.CompteComptable;
 import com.dummy.myerp.model.bean.comptabilite.EcritureComptable;
 import com.dummy.myerp.model.bean.comptabilite.JournalComptable;
 import com.dummy.myerp.model.bean.comptabilite.LigneEcritureComptable;
 import com.dummy.myerp.technical.exception.FunctionalException;
 
-@RunWith(MockitoJUnitRunner.class)
+
 public class ComptabiliteManagerImplTest {
 
     private ComptabiliteManagerImpl manager = new ComptabiliteManagerImpl();
-//    
-//    @Mock
-//    private ComptabiliteDao comptabiliteDaoMock;
-//    
-//	List<JournalComptable> listJournalExpected = new ArrayList<JournalComptable>();
-//    
-//    @Before
-//    public void setUp() {
-//     	manager.setComptabiliteDao(comptabiliteDaoMock);
-//    	JournalComptable journal1 = new JournalComptable("AC","Achat");
-//    	listJournalExpected.add(journal1);
-//    	JournalComptable journal2 = new JournalComptable("VE","Vente");
-//    	listJournalExpected.add(journal2);
-//    	JournalComptable journal3 = new JournalComptable("BQ","Banque");
-//    	listJournalExpected.add(journal3);
-//    	when(comptabiliteDaoMock.getListJournalComptable()).thenReturn(listJournalExpected);
-//    }
-//    
-//    @Test
-//    public void getListJournalComptableTest() {   	
-//    	List<JournalComptable> listJournalActual = manager.getListJournalComptable();
-//    	assertThat(listJournalActual,is(listJournalExpected));   	
-//    }
-    
-    @Test
-    public void addReferenceTest() {
-        EcritureComptable vEcritureComptable;
-        vEcritureComptable = new EcritureComptable();
-        vEcritureComptable.setJournal(new JournalComptable("AC", "Achat"));
-        vEcritureComptable.setDate(new Date());
-        vEcritureComptable.setLibelle("Libelle");
-        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(1),
-                                                                                 null, new BigDecimal(123),
-                                                                                 null));
-        vEcritureComptable.getListLigneEcriture().add(new LigneEcritureComptable(new CompteComptable(2),
-                                                                                 null, null,
-                                                                                 new BigDecimal(123)));
-        manager.addReference(vEcritureComptable);
-    	
-    }
+
 
     @Test
     public void checkEcritureComptableUnit() throws Exception {
